c80cd248bc7e9ff871f0a8964026d93386a13714
SC0d3r <sasan.kelishani@gmail.com>
2019-07-14 00:07:28 +0430

    add solve and reset buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-const WIDTH = HEIGHT = 600;
-const GLOBAL = { table: {} }
+const WIDTH = HEIGHT = 500;
+const GLOBAL = { table: {}, solve: false, emptyCells: [] }
+const solveBtn = document.querySelector('#solve-btn');
+const resetBtn = document.querySelector('#reset-btn');
+solveBtn.addEventListener('click', onClick.bind(null, solveBtn, resetBtn));
+resetBtn.addEventListener('click', onReset.bind(null, resetBtn));
+
 var loadedValues;
 function preload() {
   loadJSON('./table.easy.json', data => {
@@ -12,15 +17,9 @@ function setup_world() {
   let sudokuTable = createSudokuTable(9, 600);
   if (loadedValues) {
     sudokuTable = setValues(loadedValues, sudokuTable);
-    const emptCells = R.compose(
-      emptyCells
-    )(sudokuTable);
-
     GLOBAL.table = sudokuTable;
-    GLOBAL.emptyCells = emptCells;
   } else
     console.error('initialize table')
-
 }
 
 function setup() {
@@ -31,36 +30,52 @@ function setup() {
 function draw() {
   background(255);
   let { table, emptyCells } = GLOBAL;
-  if (!isSolved(table, emptyCells))
+  if (!isSolved(table, emptyCells) && GLOBAL.solve)
     [table, emptyCells] = guess(GLOBAL.table, GLOBAL.emptyCells)
+  else
+    onFinished(solveBtn, resetBtn);
   GLOBAL.table = table;
   GLOBAL.emptyCells = emptyCells;
   drawTable(table);
 }
 
-
-function mouseClicked() {
-  // const [clickedCellRow, clickedCellColumn] =
-  //   sudokuTable.onClickCell(mouseX, mouseY);
-  // const number = +prompt('Enter Number : ');
-  // let cellOwnNumber = sudokuTable.getCell(clickedCellRow, clickedCellColumn);
-  // cellOwnNumber = typeof cellOwnNumber === 'number' ? cellOwnNumber : cellOwnNumber.num;
-  // const finalNum = typeof number === 'number' && !isNaN(number) ? number : cellOwnNumber;
-  // // console.log(finalNum);
-  // sudokuTable.setCell(finalNum, clickedCellRow, clickedCellColumn);
-
+function isWithin(lower , upper){
+  return R.both(R.gte(R.__ , lower) , R.lte(R.__ , upper));
 }
 
-function keyPressed() {
-  // console.log(keyCode);
-  if (keyCode === 18) {//alt
-    // sudokuTable.empty();
-  }
-  if (keyCode === 32) {//spacebar
-    beginSolve();
-  }
-  if (keyCode === 17) {//ctrl
-    // const jsonTable = { table: sudokuTable.getTable() };
-    // saveJSON(jsonTable, `table.${Date.now()}.json`);
-  }
+function mouseClicked() {
+  const [clickedRow, clickedCol] = calculateCellRowCol(mouseX, mouseY);
+  const within0to8 = isWithin(0,8);
+  if(!within0to8(clickedRow) || !within0to8(clickedCol)) return;
+
+  const number = +prompt('Enter Number : ');
+  if (Number.isNaN(number)) return alert('Enter a number!');
+  GLOBAL.table = setCell(number, clickedRow, clickedCol, GLOBAL.table);
+}
+
+function calculateCellRowCol(mouseX, mouseY) {
+  const cellWidth = cellDim(GLOBAL.table);
+  const clickedCol = Math.floor((mouseX - 10) / cellWidth);
+  const clickedRow = Math.floor((mouseY - 10) / cellWidth);
+  return [clickedRow, clickedCol];
+}
+
+function onReset() {
+  GLOBAL.table = createSudokuTable(9, 600);
+}
+
+function onClick(btn, resetBtn) {
+  GLOBAL.emptyCells = emptyCells(GLOBAL.table);
+  
+  btn.textContent = 'Solving ...';
+  btn.disabled = true;
+  resetBtn.disabled = true;
+  GLOBAL.solve = true;
+}
+
+function onFinished(btn, resetBtn) {
+  btn.textContent = 'Solve';
+  GLOBAL.solve = false;
+  btn.disabled = false;
+  resetBtn.disabled = false;
 }
